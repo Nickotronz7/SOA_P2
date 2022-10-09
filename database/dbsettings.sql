bd0792d3b0f1d94ca709470001148c814a8fb4a9
Nickolas J. Rodríguez Cordero <nickotronz7@gmail.com>
2022-10-08 18:13:12 -0600

    Revert "Database + backend done and contenirized"

--- database/dbsettings.sql
+++
@@ -1,9 +0,0 @@
--- Active: 1665102474961@@127.0.0.1@3306@db_emotions
-CREATE TABLE EMPLEADO_EMOTIONS(  
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Primary Key',
-    create_time DATETIME COMMENT 'Create Time',
-    name VARCHAR(255),
-    emotion VARCHAR(255)
-) COMMENT '';
-
-INSERT INTO EMPLEADO_EMOTIONS (create_time, name, emotion) VALUES ("2006-12-31 13:25:55", 'Test_empleado', 'Test_emotion')
