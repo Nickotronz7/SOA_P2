b30cdd48ba6388b35b4274ed8d14ebd30e9119f8
Calquito <39011234+Calquito@users.noreply.github.com>
2022-10-15 15:55:32 -0600

    migrate addui to flask

--- addUI/admin files/python_webapp/main.py
+++ addUI/admin files/python_webapp/main.py
@@ -3,6 +3,7 @@ from flask import request
 from flask import redirect
 from event_processor import process_publisher
 from datetime import datetime
+import os
 import base64
 
 app = Flask(__name__)
@@ -155,4 +156,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
