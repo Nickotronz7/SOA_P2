43a5f56f4e636c2282a75d3c9340c60d9159cfbb
Kevin Acevedo Rodriguez <keviinar51@gmail.com>
2022-10-15 16:10:30 -0600

    ResultsUI yaml file added

---
+++ resultsUI/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import React from 'react'
+
+import EmployeesData from "./components/employeesData";
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeesData />
+    </div>
+  );
+}
+
+export default App;
