e2806eb891106282415e65872880c191c9138403
Pablouc <lpablouc9gmail.com>
2022-10-15 22:25:35 -0600

    http GET request verified

--- resultsUI/src/components/employeesData.js
+++ resultsUI/src/components/employeesData.js
@@ -1,6 +1,5 @@
 import React, {Component } from 'react'
 import axios from 'axios'
-import data from '../assets/data.json'
 
 class EmployeesData extends Component{
     constructor(props) {
@@ -16,7 +15,7 @@ class EmployeesData extends Component{
     componentDidMount() {
         axios.get('http://api:10000/allrecords')
         .then(response => {
-            console.log(response)
+            console.log(response.data)
             this.setState({employees_data: response.data})
         })
         .catch(error => {
@@ -32,7 +31,7 @@ class EmployeesData extends Component{
         const{ employees_data} = this.state
         return (
             <header className="App-header">
-            <button class="button-32" role="button" onClick={this.refresh}>Refresh</button>
+            <button className="button-32" onClick={this.refresh}>Refresh</button>
             <h1>Employees Emotions</h1>
             <div className="header">
                 <p className="name" >Name</p>
@@ -42,12 +41,12 @@ class EmployeesData extends Component{
             <div className="container">
             
             
-            {employees_data.response.map((element,index) => {
+            {employees_data.map((element,index) => {
                 return (
                 <div key={index} className="tableLike">
-                    <p className="name">{element.title}</p>
-                    <p className="emotion">{element.body}</p>
-                    <p className="date">{element.id}</p>
+                    <p className="name">{element.nombre}</p>
+                    <p className="emotion">{element.emocion}</p>
+                    <p className="date">{element.fecha}</p>
                 </div>
                 );
             })}
